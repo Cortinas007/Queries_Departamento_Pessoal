298b446d2b5ea9950aa392218f527220f252b101
Andrey Rodrigues Leite <andreytca1911@gmail.com>
2026-05-28 16:20:58 -0300

    Update Query_Historico_Salario_Cargos.sql

--- Query_Historico_Salario_Cargos.sql
+++ Query_Historico_Salario_Cargos.sql
@@ -1,4 +1,11 @@
-
+A empresa utiliza uma plataforma web integrada diretamente ao banco de dados do ERP corporativo para fornecer relatórios de Business Partner 
+   em tempo real para os clientes.
+    Sempre que uma nova empresa é integrada/cadastrada no banco de dados, o histórico de alterações salariais dos colaboradores é importado em formato bruto. 
+    O sistema nativo não calcula retroativamente o percentual de aumento de cada dissídio ou promoção ao longo dos anos, defasando a ficha financeira dos colaboradores.
+Isso quebrava a automação API que alimenta o site.
+Para eliminar este retrabalho, desenvolvi uma consulta em PostgreSQL que utiliza Funções de Janela (LAG e OVER). 
+    A inteligência da query analisa o histórico salarial cronológico de cada colaborador de forma automatizada, 
+    detecta o salário anterior na linha de cima e calcula o percentual exato de aumento em tempo real diretamente na fonte de dados.
 
 SELECT
     colab.id_filial AS "Filial",
@@ -13,7 +20,7 @@ SELECT
     -- CÁLCULO DE PERCENTUAL DE AUMENTO USANDO FUNÇÃO DE JANELA (LAG)
     ROUND(
         (
-            (hist_sal.valor_salario / NULLIF(
+            (hist_sal.valor_salario / NULLIF(          --- USO DO NULLIF PARA CASO HAJA ALGUM COLABORADOR QUE NÃO POSSUA HISTÓRICO DE SALARIO A CONSULTA NÃO QUEBRAR DIVIDINDO POR 0 ---
                 LAG(hist_sal.valor_salario) OVER(
                     PARTITION BY hist_sal.id_empresa, hist_sal.id_colaborador 
                     ORDER BY hist_sal.data_alteracao ASC
